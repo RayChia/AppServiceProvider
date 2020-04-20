75eb73bfb8f57531bba5bf095035ed63ff83d574
Raychia92 <ray76523@gmail.com>
2020-04-20 10:28:20 +0800

    補一些

---
+++ AppServiceProvider/Models/News/SlideShow.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace AppServiceProvider.Models
+{
+    public class SlideShow
+    {
+        public string id { get; set; } = "";
+        public string pic { get; set; } = "";
+        public string storename { get; set; } = "";
+        public string customerID { get; set; } = "";
+        public string missionrecommand { get; set; } = "";
+    }
+}
